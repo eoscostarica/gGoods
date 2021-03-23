fd814eaa325c3c4e6a617584305889cdf9cedd9b
JeanVegaD <jean0798@gmail.com>
2021-03-23 16:08:24 -0600

    update goods page

--- webapp/src/routes/Goods/index.js
+++ webapp/src/routes/Goods/index.js
@@ -2,13 +2,44 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/styles'
+import Grid from '@material-ui/core/Grid'
+
+import { CardAvatar } from '../../components/Card'
+import styles from './styles'
+
+const useStyles = makeStyles(styles)
+
+const GamesList = [
+  {
+    name: 'Selfie Cam'
+  },
+  {
+    name: 'Free your Animal'
+  }
+]
 
 const Goods = () => {
-  const { t } = useTranslation('homeRoute')
+  const classes = useStyles()
+  const { t } = useTranslation('goodsRoute')
 
   return (
     <Box>
-      <Typography>{t('welcomeMessage')}</Typography>
+      <Typography variant="h4" className={classes.titlePage}>
+        {t('title')}
+      </Typography>
+      <Typography className={classes.textPageDescription}>
+        {t('paragraph1')}
+      </Typography>
+      <Box>
+        <Grid container spacing={2}>
+          {GamesList.map(game => (
+            <Grid item xs={12} md={6} lg={3} key={game.name}>
+              <CardAvatar />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   )
 }
---
+++ webapp/src/routes/Goods/styles.js
@@ -0,0 +1,8 @@
+export default theme => ({
+  titlePage: {
+    marginBottom: theme.spacing(3)
+  },
+  textPageDescription: {
+    marginBottom: theme.spacing(2)
+  }
+})
