329ef95a0d93de5447bfe501fa79340bc1c26571
kriskoin <cris7775@gmail.com>
2021-03-23 17:57:32 -0600

    wip: update

--- contracts/chgname.sh
+++ contracts/chgname.sh
@@ -7,14 +7,16 @@
 # version 1.0.0
 #
 #  DESCRIPTION
-#    the main propouse of this script is create a new smart contract  
-#    based on https://github.com/MythicalGames/dgoods
+#    the main propouse of this script is to be used in 
+#    offline bare-metal setups. It create a new smart contract  
+#    based on https://github.com/MythicalGames/dgoods, 
+#    it must run on contracts folder
 #
 
+# New contract name
 CONTRACT_NAME='ggoodsggoods'
 
 # clone dgoods directory
-
 cp dgoods $CONTRACT_NAME -rf
 cd $CONTRACT_NAME
 
@@ -33,10 +35,37 @@ sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" Makefile
 sed -i'.old' "s/dgoods* /"$CONTRACT_NAME"/" Makefile
 
 #example_usage.md
-
 sed -i'.old' "s/dgood.token/"$CONTRACT_NAME"/" example_usage.md
 sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" example_usage.md
 
-
 #CMakeLists.txt
 sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" CMakeLists.txt
+
+# /src/CMakeLists.txt
+cd src
+sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" CMakeLists.txt
+sed -i'.old' "s/dgoods dgoods.cpp/"$CONTRACT_NAME" "$CONTRACT_NAME".cpp/" CMakeLists.txt
+
+# /src/dgoods.cpp change name
+mv dgoods.cpp $CONTRACT_NAME.cpp
+sed -i'.old' "s/<dgoods.hpp>/<"$CONTRACT_NAME".hpp>/" $CONTRACT_NAME.cpp
+sed -i'.old' "s/dgoods::/"$CONTRACT_NAME"::/" $CONTRACT_NAME.cpp
+
+#get out of /src
+rm *.old
+cd ..
+
+# /ricardian/dgoods.contracts.md change name
+cd ricardian
+mv dgoods.contracts.md $CONTRACT_NAME.contracts.md
+
+# /include/dgoods.hpp change name
+cd ../include
+mv dgoods.hpp $CONTRACT_NAME.hpp
+sed -i'.old' "s/dgoods(/"$CONTRACT_NAME"(/" $CONTRACT_NAME.hpp
+sed -i'.old' "s/dgoods:/"$CONTRACT_NAME":/" $CONTRACT_NAME.hpp
+
+#get out of /include
+rm *.old
+cd ..
+rm *.old
