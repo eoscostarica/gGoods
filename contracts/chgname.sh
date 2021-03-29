0ad2730f4d64a8c16431f874ec60127febb658b2
kriskoin <cris7775@gmail.com>
2021-03-28 19:23:00 -0600

    fix(smart-contracts): delete quotes

--- contracts/chgname.sh
+++ contracts/chgname.sh
@@ -25,32 +25,32 @@ mv dgoods_spec.md  ${CONTRACT_NAME}_spec.md
 
 #change name references
 # README.md
-sed -i'.old' "s/(dgoods_spec.md)/("$CONTRACT_NAME"_spec.md)/" README.md
+sed -i.old "s/(dgoods_spec.md)/("$CONTRACT_NAME"_spec.md)/" README.md
 
 # Makefile
-sed -i'.old' "s/dgoods.abi/"$CONTRACT_NAME".abi/" Makefile
-sed -i'.old' "s/dgoods.wasm/"$CONTRACT_NAME".wasm/" Makefile
-sed -i'.old' "s/dgoods.abi/"$CONTRACT_NAME".abi/" Makefile
-sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" Makefile
-sed -i'.old' "s/dgoods* /"$CONTRACT_NAME"/" Makefile
+sed -i.old "s/dgoods.abi/"$CONTRACT_NAME".abi/" Makefile
+sed -i.old "s/dgoods.wasm/"$CONTRACT_NAME".wasm/" Makefile
+sed -i.old "s/dgoods.abi/"$CONTRACT_NAME".abi/" Makefile
+sed -i.old "s/dgoods/"$CONTRACT_NAME"/" Makefile
+sed -i.old "s/dgoods* /"$CONTRACT_NAME"/" Makefile
 
 #example_usage.md
-sed -i'.old' "s/dgood.token/"$CONTRACT_NAME"/" example_usage.md
-sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" example_usage.md
+sed -i.old "s/dgood.token/"$CONTRACT_NAME"/" example_usage.md
+sed -i.old "s/dgoods/"$CONTRACT_NAME"/" example_usage.md
 
 #CMakeLists.txt
-sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" CMakeLists.txt
+sed -i.old "s/dgoods/"$CONTRACT_NAME"/" CMakeLists.txt
 
 # /src/CMakeLists.txt
 cd src
-sed -i'.old' "s/dgoods/"$CONTRACT_NAME"/" CMakeLists.txt
-sed -i'.old' "s/dgoods dgoods.cpp/"$CONTRACT_NAME" "$CONTRACT_NAME".cpp/" CMakeLists.txt
+sed -i.old "s/dgoods/"$CONTRACT_NAME"/" CMakeLists.txt
+sed -i.old "s/dgoods dgoods.cpp/"$CONTRACT_NAME" "$CONTRACT_NAME".cpp/" CMakeLists.txt
 
 # /src/dgoods.cpp change name
 mv dgoods.cpp $CONTRACT_NAME.cpp
-sed -i'.old' "s/<dgoods.hpp>/<"$CONTRACT_NAME".hpp>/" $CONTRACT_NAME.cpp
-sed -i'.old' "s/dgoods::/"$CONTRACT_NAME"::/" $CONTRACT_NAME.cpp
-sed -i'.old' "s/dgoods,/"$CONTRACT_NAME",/" $CONTRACT_NAME.cpp
+sed -i.old "s/<dgoods.hpp>/<"$CONTRACT_NAME".hpp>/" $CONTRACT_NAME.cpp
+sed -i.old "s/dgoods::/"$CONTRACT_NAME"::/" $CONTRACT_NAME.cpp
+sed -i.old "s/dgoods,/"$CONTRACT_NAME",/" $CONTRACT_NAME.cpp
 #get out of /src
 rm *.old
 cd ..
@@ -62,8 +62,8 @@ mv dgoods.contracts.md $CONTRACT_NAME.contracts.md
 # /include/dgoods.hpp change name
 cd ../include
 mv dgoods.hpp $CONTRACT_NAME.hpp
-sed -i'.old' "s/dgoods(/"$CONTRACT_NAME"(/" $CONTRACT_NAME.hpp
-sed -i'.old' "s/dgoods:/"$CONTRACT_NAME":/" $CONTRACT_NAME.hpp
+sed -i.old "s/dgoods(/"$CONTRACT_NAME"(/" $CONTRACT_NAME.hpp
+sed -i.old "s/dgoods:/"$CONTRACT_NAME":/" $CONTRACT_NAME.hpp
 
 #get out of /include
 rm *.old
