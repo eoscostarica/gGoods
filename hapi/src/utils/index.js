64f60b4bb1ea295bc3dc751ddf9d9314c1ef8ed3
Adriel Diaz <adriexnet@gmail.com>
2021-03-31 17:30:39 -0600

    feat(hapi): add paypal util with options to get access token and order info

--- hapi/src/utils/index.js
+++ hapi/src/utils/index.js
@@ -6,5 +6,6 @@ module.exports = {
   ipfsUtil: require('./ipfs.util'),
   jwtUtil: require('./jwt.util'),
   mailUtil: require('./mail'),
+  paypalUtil: require('./paypal.util'),
   walletUtil: require('./wallet.util')
 }
