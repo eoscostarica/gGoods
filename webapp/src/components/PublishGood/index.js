1afbe3e57a183f059b5d0e37c5659547a0b7cd08
Adriel Diaz <adriexnet@gmail.com>
2021-03-31 23:07:08 -0600

    feat(webapp): implement real data from database for inventory page

--- webapp/src/components/PublishGood/index.js
+++ webapp/src/components/PublishGood/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles, useTheme } from '@material-ui/styles'
 import { useTranslation } from 'react-i18next'
@@ -12,22 +12,66 @@ import CloseIcon from '@material-ui/icons/Close'
 import Backdrop from '@material-ui/core/Backdrop'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { useMutation } from '@apollo/client'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+import { CardAvatar } from '../Card'
+import { PUT_ON_SALE_MUTATION } from '../../gql'
+import { useSharedState } from '../../context/state.context'
 
 import styles from './styles'
-import { CardAvatar } from '../Card'
 
 const useStyles = makeStyles(styles)
+const initialValue = {
+  quantity: '',
+  amount: '',
+  donable: false
+}
 
-const PublishGood = ({ open, handlerOpen }) => {
+const PublishGood = ({ open, template, onClose }) => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const [putOnSale, { loading }] = useMutation(PUT_ON_SALE_MUTATION)
   const classes = useStyles()
   const { t } = useTranslation('publishGood')
+  const [payload, setPayload] = useState()
+  const [, { showMessage }] = useSharedState()
+
+  const handlePayloadChange = field => event => {
+    setPayload(prev => ({ ...prev, [field]: event.target.value }))
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const { data: response } = await putOnSale({
+        variables: {
+          ...payload,
+          template: template.id,
+          amount: parseFloat(payload.amount),
+          quantity: parseInt(payload.quantity)
+        }
+      })
+      setPayload(initialValue)
+      onClose()
+      showMessage({
+        content: `${t('successMessage')} ${response.sale.assets.join(',')}`
+      })
+    } catch (error) {
+      showMessage({ type: 'error', content: error.message })
+    }
+  }
+
+  useEffect(() => {
+    setPayload(initialValue)
+  }, [])
 
   return (
     <Dialog
       open={open}
-      onClose={handlerOpen}
+      onClose={onClose}
       aria-labelledby="transition-modal-title"
       aria-describedby="transition-modal-description"
       fullScreen={isMobile}
@@ -45,7 +89,7 @@ const PublishGood = ({ open, handlerOpen }) => {
             aria-label="close"
             color="inherit"
             size="small"
-            onClick={handlerOpen}
+            onClick={onClose}
           >
             <CloseIcon fontSize="inherit" />
           </IconButton>
@@ -59,7 +103,11 @@ const PublishGood = ({ open, handlerOpen }) => {
         <Box className={classes.sectionBoxDouble}>
           <Grid container justify="center">
             <Grid item xs={6}>
-              <CardAvatar name={'Lola the Jaguar'} />
+              <CardAvatar
+                name={template?.name}
+                image={template?.image}
+                backgroundColor={template?.backgroundColor}
+              />
             </Grid>
           </Grid>
         </Box>
@@ -68,11 +116,32 @@ const PublishGood = ({ open, handlerOpen }) => {
             label={t('amount')}
             variant="filled"
             className={classes.textField}
+            value={payload?.quantity || ''}
+            onChange={handlePayloadChange('quantity')}
           />
           <TextField
             label={t('suggestedPrice')}
             variant="filled"
             className={classes.textField}
+            value={payload?.amount || ''}
+            onChange={handlePayloadChange('amount')}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              )
+            }}
+          />
+          <FormControlLabel
+            label={t('allowDonation')}
+            control={
+              <Switch
+                checked={payload?.donable || false}
+                onChange={(_, value) =>
+                  handlePayloadChange('donable')({ target: { value } })
+                }
+                color="primary"
+              />
+            }
           />
         </Box>
         <Box className={classes.sectionBox}>
@@ -80,8 +149,13 @@ const PublishGood = ({ open, handlerOpen }) => {
             variant="contained"
             color="primary"
             className={classes.mainButton}
+            onClick={handleSubmit}
+            disabled={
+              loading || !template?.id || !payload?.amount || !payload?.quantity
+            }
           >
-            {t('createGood')}
+            {!loading && t('createGood')}
+            {loading && <CircularProgress />}
           </Button>
         </Box>
       </Box>
@@ -91,7 +165,8 @@ const PublishGood = ({ open, handlerOpen }) => {
 
 PublishGood.propTypes = {
   open: PropTypes.bool,
-  handlerOpen: PropTypes.func
+  onClose: PropTypes.func,
+  template: PropTypes.any
 }
 
 export default PublishGood
